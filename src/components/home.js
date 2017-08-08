4493e423890fde338a62738d2b6912b08bc0e8b7
Bernard Laveaux <blaveaux@wealthsimple.com>
2017-08-08 09:49:35 -0400

    Use forked polyglot plugin:
    Initial implementation of bio

--- src/components/home.js
+++ src/components/home.js
@@ -5,21 +5,32 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.t = this.t.bind(this);
+    console.log(this.t('home.bio.content'));
     this.state = {
       title: this.t('home.title'),
-      bio: this.t('home.bio'),
+      bio: {
+        title: this.t('home.bio.title'),
+        // TODO: Componentize the following hrefs
+        content: this.t('home.bio.content', {
+          carleton: "<href>Carleton University</href>",
+          wealthsimple: "<a href='https://www.wealthsimple.com'>Wealthsimple</a>",
+          shopify: "<href>Shopify</href>",
+          ibm: "<href>IBM</href>"
+        }),
+      }
     };
   }
 
-  t(symbol) {
-    return this.props.translate.t(symbol);
+  t(symbol, opts = {}) {
+    return this.props.translate.t(symbol, opts);
   }
 
   render() {
     return (
       <div className="Home">
         <div className='text title' >Bernard Laveaux</div>
-        <div className='text bio' >{this.state.bio}</div>
+        <div className='text bio' >{this.state.bio.title}</div>
+        <div className='text content' >{this.state.bio.content}</div>
         <div className='social'>
           <img className="text" alt="Github" src={"/github-alt.svg"}/>
         </div>
